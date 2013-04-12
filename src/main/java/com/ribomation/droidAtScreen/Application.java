8c6d3791d16674ec7701361d3153c8daf561fa07
kumabotz <kumabotz@gmail.com>
2013-04-13 02:06:01 +0800

    Add get preferences

--- src/main/java/com/ribomation/droidAtScreen/Application.java
+++ src/main/java/com/ribomation/droidAtScreen/Application.java
@@ -1,7 +1,13 @@
 package com.ribomation.droidAtScreen;
 
-import com.ribomation.droidAtScreen.dev.AndroidDevice;
-// TODO
-public interface Application {
+//import com.ribomation.droidAtScreen.dev.AndroidDevice;
 
+import java.util.prefs.Preferences;
+
+/**
+ * Application interface that provides a set of services for disparate parts of
+ * the app.
+ */
+public interface Application {
+  Preferences getPreferences();
 }
