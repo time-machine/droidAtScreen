5dc0d56a833ee8055e73f9deeb26bf2bdf234b9b
kumabotz <kumabotz@gmail.com>
2013-04-13 02:08:58 +0800

    Impletment application interface, add init properties

--- src/main/java/com/ribomation/droidAtScreen/DroidAtScreenApplication.java
+++ src/main/java/com/ribomation/droidAtScreen/DroidAtScreenApplication.java
@@ -2,16 +2,63 @@ package com.ribomation.droidAtScreen;
 
 import org.apache.log4j.Logger;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.*;
+import java.util.prefs.BackingStoreException;
+import java.util.prefs.Preferences;
 
-public class DroidAtScreenApplication {
+public class DroidAtScreenApplication implements Application {
   private Logger log = Logger.getLogger(DroidAtScreenApplication.class);
+  private Preferences appPreferences;
+  private final String appPropertiesPath = "/META-INF/maven/com.ribomation/droidAtScreen/pom.properties";
+  private String appVersion = "0.1";
+
   public static void main(String[] args) {
     DroidAtScreenApplication app = new DroidAtScreenApplication();
     app.parseArgs(args);
+    app.initProperties();
   }
 
   private void parseArgs(String[] args) {
     log.debug("parseArgs: " + Arrays.toString(args));
+  }
+
+  private void initProperties() {
+    log.debug("initProperties");
+    InputStream is = this.getClass().getResourceAsStream(appPropertiesPath);
+    if (is != null) {
+      try {
+        Properties prp = new Properties();
+        prp.load(is);
+        appVersion = prp.getProperty("version", appVersion);
+      }
+      catch (IOException e) {
+        log.debug("Missing classpath resource: " + appPropertiesPath, e);
+      }
+    }
+
+    try {
+      log.debug("--- Preferences ---");
+      Preferences prefs = getPreferences();
+      for (String key : prefs.keys()) {
+        log.debug(String.format("%s: %s", key, prefs.get(key, "[none]")));
+      }
+      log.debug("--- END ---");
+    }
+    catch (BackingStoreException e) {
+      log.warn("Failed to list prefs", e);
+    }
+  }
+
+  // --------------------------------------------
+  // Application
+  // --------------------------------------------
+  @Override
+  public Preferences getPreferences() {
+    if (appPreferences == null) {
+      appPreferences = Preferences.userNodeForPackage(this.getClass());
+    }
+    return appPreferences;
   }
 }
