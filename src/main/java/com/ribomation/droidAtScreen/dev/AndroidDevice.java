80463b6e5ec62256e7e4559201d5b6728f04b594
kumabotz <kumabotz@gmail.com>
2013-04-13 02:07:06 +0800

    Clean up

--- src/main/java/com/ribomation/droidAtScreen/dev/AndroidDevice.java
+++ src/main/java/com/ribomation/droidAtScreen/dev/AndroidDevice.java
@@ -1,12 +1,9 @@
 package com.ribomation.droidAtScreen.dev;
 
-import java.awt.image.BufferedImage;
+//import java.awt.image.BufferedImage;
 
 /**
  * Facade/wrapper around an Android device.
- *
- * @user jens
- * @date 2010-jan-17 12:16:55
  */
 public interface AndroidDevice {
 
