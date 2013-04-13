3180c38f0e51fec3e8609e325a2268b4b1b26591
kumabotz <kumabotz@gmail.com>
2013-04-13 22:50:31 +0800

    Add adb and device changed listener

--- src/main/java/com/ribomation/droidAtScreen/dev/AndroidDeviceManager.java
+++ src/main/java/com/ribomation/droidAtScreen/dev/AndroidDeviceManager.java
@@ -1,13 +1,62 @@
 package com.ribomation.droidAtScreen.dev;
 
 import com.android.ddmlib.AndroidDebugBridge;
+import com.android.ddmlib.IDevice;
+import org.apache.log4j.Logger;
 
 /**
  * A facade to AndroidDebugBridge.
  */
-public class AndroidDeviceManager {
+public class AndroidDeviceManager extends Thread implements
+    AndroidDebugBridge.IDebugBridgeChangeListener,
+    AndroidDebugBridge.IDeviceChangeListener {
+  private Logger log = Logger.getLogger(this.getClass());
+
   public AndroidDeviceManager() {
     AndroidDebugBridge.init(false);
+    Runtime.getRuntime().addShutdownHook(this);
+
+    AndroidDebugBridge.addDebugBridgeChangeListener(this);
+    AndroidDebugBridge.addDeviceChangeListener(this);
+  }
+
+  @Override
+  public void bridgeChanged(AndroidDebugBridge adb) {
+    this.adb = adb;
+    log.debug("ADB changed");
+  }
+
+  @Override
+  public void deviceChanged(IDevice dev, int changeMask) {
+    log.debug("Device changed: " + dev + ", mask=" + toMaskString(changeMask));
+  }
+
+  @Override
+  public void deviceDisconnected(IDevice target) {
     //
   }
+
+  @Override
+  public void deviceConnected(IDevice target) {
+    //
+  }
+
+  protected String toMaskString(int mask) {
+    StringBuilder result = new StringBuilder("");
+    if ((mask & IDevice.CHANGE_BUILD_INFO) != 0) {
+      result.append("CHANGE_BUILD_INFO ");
+    }
+    if ((mask & IDevice.CHANGE_CLIENT_LIST) != 0) {
+      result.append("CHANGE_CLIENT_LIST ");
+    }
+    if ((mask & IDevice.CHANGE_STATE) != 0) {
+      result.append("CHANGE_STATE ");
+    }
+    return result.toString();
+  }
+
+  /**
+   * The ADB object.
+   */
+  private AndroidDebugBridge adb;
 }
