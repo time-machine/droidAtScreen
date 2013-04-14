b3ce9bbf735b0dce49b3f394fee95e3d3a2d9885
kumabotz <kumabotz@gmail.com>
2013-04-14 16:48:32 +0800

    Initial commit

---
+++ src/main/java/com/ribomation/droidAtScreen/cmd/QuitCommand.java
@@ -0,0 +1,27 @@
+package com.ribomation.droidAtScreen.cmd;
+
+import com.ribomation.droidAtScreen.Application;
+
+import javax.swing.*;
+
+public class QuitCommand extends Command {
+  public QuitCommand() {
+    setLabel("Quit");
+    setTooltip("Quits the application");
+    // setIcon("quit_16x16");
+    setMnemonic('Q');
+  }
+
+  @Override
+  protected void doExecute(Application app) {
+    int rc = JOptionPane.showConfirmDialog(app.getAppFrame(),
+        "Do you really want to quit?", "Quit?", JOptionPane.OK_CANCEL_OPTION);
+    if (rc == JOptionPane.OK_OPTION) {
+      JFrame f = app.getAppFrame();
+      if (f != null) {
+        f.dispose();
+      }
+      System.exit(0);
+    }
+  }
+}
