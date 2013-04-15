b9b87850dc229109a43ca740c78176814ad52439
kumabotz <kumabotz@gmail.com>
2013-04-15 22:19:58 +0800

    Add connected and disconnected

--- src/main/java/com/ribomation/droidAtScreen/dev/AndroidDeviceListener.java
+++ src/main/java/com/ribomation/droidAtScreen/dev/AndroidDeviceListener.java
@@ -4,5 +4,15 @@ package com.ribomation.droidAtScreen.dev;
  * Notification of connected and disconnected devices.
  */
 public interface AndroidDeviceListener {
-  //
+  /**
+   * Invoked when a new device is detected.
+   * @param dev the new device
+   */
+  void connected(AndroidDevice dev);
+
+  /**
+   * Invoked when a device goes offline.
+   * @param dev the defunct device
+   */
+  void disconnected(AndroidDevice dev);
 }
