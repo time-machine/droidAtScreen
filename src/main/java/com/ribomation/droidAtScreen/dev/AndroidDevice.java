47431eb5fa226ebd4665488c03b3473931a24da4
kumabotz <kumabotz@gmail.com>
2013-04-21 19:11:58 +0800

    Add get name

--- src/main/java/com/ribomation/droidAtScreen/dev/AndroidDevice.java
+++ src/main/java/com/ribomation/droidAtScreen/dev/AndroidDevice.java
@@ -6,5 +6,9 @@ package com.ribomation.droidAtScreen.dev;
  * Facade/wrapper around an Android device.
  */
 public interface AndroidDevice {
-
+  /**
+   * Returns its device name.
+   * @return its name
+   */
+  String getName();
 }
