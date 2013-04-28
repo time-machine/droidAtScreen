7bda660b06d3d1c135ef79454986b86cb14a7c24
kumabotz <kumabotz@gmail.com>
2013-04-28 15:18:27 +0800

    Add get screen shot

--- src/main/java/com/ribomation/droidAtScreen/dev/AndroidDeviceImpl.java
+++ src/main/java/com/ribomation/droidAtScreen/dev/AndroidDeviceImpl.java
@@ -1,6 +1,9 @@
 package com.ribomation.droidAtScreen.dev;
 
 import com.android.ddmlib.IDevice;
+import com.android.ddmlib.RawImage;
+
+import java.awt.image.BufferedImage;
 
 /**
  * Implementation of {@link com.ribomation.droidAtScreen.dev.AndroidDevice},
@@ -23,5 +26,35 @@ public class AndroidDeviceImpl implements AndroidDevice {
     return target.isEmulator();
   }
 
-  // TODO: -
+  @Override
+  public BufferedImage getScreenShot() {
+    return getScreenShot(false);
+  }
+
+  @Override
+  public BufferedImage getScreenShot(boolean landscapeMode) {
+    try {
+      RawImage screenShot = target.getScreenshot();
+      if (screenShot == null) return null;
+      if (landscapeMode) {
+        screenShot = screenShot.getRotated();
+      }
+
+      BufferedImage image = new BufferedImage(screenShot.width,
+          screenShot.height, BufferedImage.TYPE_INT_ARGB);
+      int pixelIndexIncrement = screenShot.bpp >> 3;
+
+      for (int y = 0, pixelIdx = 0; y < screenShot.height; y++) {
+        for (int x = 0; x < screenShot.width; x++,
+            pixelIdx += pixelIndexIncrement) {
+          image.setRGB(x, y, screenShot.getARGB(pixelIdx));
+        }
+      }
+
+      return image;
+    }
+    catch (Exception e) {
+      throw new RuntimeException("Failed to capture device screen shot: " + e);
+    }
+  }
 }
