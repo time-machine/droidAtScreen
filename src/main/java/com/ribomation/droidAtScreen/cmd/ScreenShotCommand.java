1dfcda82457f4a631e8f04fc61177f1452df1920
kumabotz <kumabotz@gmail.com>
2013-04-29 21:33:36 +0800

    Add do execute

--- src/main/java/com/ribomation/droidAtScreen/cmd/ScreenShotCommand.java
+++ src/main/java/com/ribomation/droidAtScreen/cmd/ScreenShotCommand.java
@@ -3,9 +3,17 @@ package com.ribomation.droidAtScreen.cmd;
 import com.ribomation.droidAtScreen.Application;
 import com.ribomation.droidAtScreen.dev.AndroidDevice;
 
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import javax.swing.filechooser.FileNameExtensionFilter;
 import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
 public class ScreenShotCommand extends Command {
+  private static final String JPG = "jpg";
+  private File lastFile = null;
+
   public ScreenShotCommand() {
     setLabel("Save ScreenShot as JPG");
     setTooltip("Takes a screen-shot of the current device and saves it as " +
@@ -19,6 +27,36 @@ public class ScreenShotCommand extends Command {
     if (dev == null) return;
 
     final BufferedImage screenShot = dev.getScreenShot();
-    // TOOD: what's next?
+    JFileChooser chooser = new JFileChooser(lastFile);
+    chooser.setSelectedFile(lastFile != null? lastFile :
+        new File("droidAtScreen.jpg"));
+    chooser.setFileSelectionMode(JFileChooser.FILES_ONLY);
+    chooser.addChoosableFileFilter(new FileNameExtensionFilter("JPG Files",
+        "jpg"));
+
+    int rc = chooser.showSaveDialog(app.getAppFrame());
+    if (rc == JFileChooser.APPROVE_OPTION) {
+      lastFile = chooser.getSelectedFile();
+      SwingUtilities.invokeLater(new Runnable() {
+        @Override
+        public void run() {
+          try {
+            if (lastFile.exists()) {
+              int rc = JOptionPane.showConfirmDialog(app.getAppFrame(),
+                  "File '" + lastFile +
+                  "' already exist. Do you want to overwrite?",
+                  "Overwrite file", JOptionPane.YES_OPTION);
+              if (rc != JOptionPane.YES_OPTION) return;
+            }
+            ImageIO.write(screenShot, JPG, lastFile);
+          }
+          catch (IOException e) {
+            JOptionPane.showMessageDialog(app.getAppFrame(),
+                "Failed to save screen-shot file " + lastFile + ". " + e,
+                "Failed to save file", JOptionPane.ERROR_MESSAGE);
+          }
+        }
+      });
+    }
   }
 }
