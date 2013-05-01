4678d28fd0ad37329d6d37cb5a71bd753674a93b
kumabotz <kumabotz@gmail.com>
2013-05-01 22:52:52 +0800

    Initial commit

---
+++ src/main/java/com/ribomation/droidAtScreen/cmd/RemovePropertiesCommand.java
@@ -0,0 +1,23 @@
+package com.ribomation.droidAtScreen.cmd;
+
+import com.ribomation.droidAtScreen.Application;
+
+import javax.swing.*;
+
+public class RemovePropertiesCommand extends Command {
+  public RemovePropertiesCommand() {
+    setLabel("Remove application properties");
+    setTooltip("Removes all saved application properties from this host.");
+  }
+
+  @Override
+  protected void doExecute(Application app) {
+    int rc = JOptionPane.showConfirmDialog(app.getAppFrame(),
+        "Are you sure you want to remove all persisted properties of this " +
+        "application from this host?", "Remove app properties?",
+        JOptionPane.YES_NO_OPTION);
+    if (rc == JOptionPane.YES_OPTION) {
+      app.destroyPreferences();
+    }
+  }
+}
