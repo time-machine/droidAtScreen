5a6f12d9c3be324ce1c51be0d821c268e43773a4
kumabotz <kumabotz@gmail.com>
2013-05-14 15:21:26 +0800

    Initial commit

---
+++ src/main/java/com/ribomation/droidAtScreen/cmd/OrientationCommand.java
@@ -0,0 +1,43 @@
+package com.ribomation.droidAtScreen.cmd;
+
+import com.ribomation.droidAtScreen.Application;
+
+import javax.swing.*;
+
+/**
+ * Flips the device frame orientation.
+ */
+public class OrientationCommand extends CheckBoxCommand {
+  public OrientationCommand() {
+    boolean landscape = getApplication().getPreferences().getBoolean(
+        getPreferencesKey(), false);
+    updateView(landscape);
+    setTooltip("Flip the orientation (portrait | landscape)");
+    setEnabled(false);
+  }
+
+  @Override
+  protected void notifyApplication(Application app, boolean landscape) {
+    app.setPortraitMode(!landscape);
+    updateView(landscape);
+  }
+
+  private void updateView(boolean landscape) {
+    setLabel(landscape? "Landscape" : "Portrait");
+    setIcon("orientation-" + getLabel().toLowerCase());
+  }
+
+  @Override
+  protected String getPreferencesKey() {
+    return "landscape-mode";
+  }
+
+  @Override
+  public AbstractButton newButton() {
+    JToggleButton b = new JToggleButton(this);
+    b.setVerticalTextPosition(AbstractButton.BOTTOM);
+    b.setHorizontalTextPosition(AbstractButton.CENTER);
+    b.setSelected(getPreferenceValue());
+    return b;
+  }
+}
